398c73d292ed3a9f36a22fcda23e92b870717548
Moritz Friedrich <moritz@matchory.com>
2025-05-01 21:42:35 +0200

    Unified Typescript configuration

--- packages/ui/eslint.config.ts
+++ packages/ui/eslint.config.ts
@@ -1,7 +1,18 @@
 import { config } from "@colibri-hq/shared/eslint.config";
 import svelteConfig from "./svelte.config.js";
 
-export default config({
-  tsconfigRootDir: import.meta.dirname,
-  svelteConfig,
-});
+export default config(
+  {
+    tsconfigRootDir: import.meta.dirname,
+    svelteConfig,
+  },
+  {
+    files: ["src/**/*.stories.svelte"],
+    languageOptions: {
+      parserOptions: {
+        projectService: false,
+        project: "./tsconfig.storybook.json",
+      },
+    },
+  },
+) as ReturnType<typeof config>;
