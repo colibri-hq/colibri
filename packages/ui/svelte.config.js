2b8d5a712622d1fe49358265db18ced4e40c959b
Moritz Friedrich <moritz@matchory.com>
2025-05-01 21:38:55 +0200

    Enable prerendering and configure static adapter in Svelte setup

--- packages/ui/svelte.config.js
+++ packages/ui/svelte.config.js
@@ -1,12 +1,21 @@
+import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
-  preprocess: vitePreprocess(),
   compilerOptions: {
     modernAst: true,
     discloseVersion: true,
   },
+  kit: {
+    adapter: adapter({
+      pages: "dist/preview",
+      assets: "dist/preview",
+      precompress: true,
+      strict: true,
+    }),
+  },
+  preprocess: vitePreprocess(),
 };
 
 export default config;
