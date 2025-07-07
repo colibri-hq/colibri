2a73305cb90ea1540b1bf7f52b8f77f2cccf2411
Moritz Friedrich <moritz@matchory.com>
2025-07-07 10:12:15 +0200

    Added shared Colibri User Agent export

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,3 +1,5 @@
+import packageJson from '../package.json' with { type: 'json' };
+
 export * from './base64.js';
 export * from './buffer.js';
 export * from './crypto.js';
@@ -6,3 +8,11 @@ export * from './mediaType.js';
 export * from './random.js';
 export * from './types.js';
 export * from './utilities.js';
+
+/**
+ * User agent string for requests sent by Colibri.
+ *
+ * This user-agent string is used to identify requests made by Colibri to
+ * external services.
+ */
+export const userAgent = `Colibri/${packageJson.version} (${packageJson.homepage})`;
